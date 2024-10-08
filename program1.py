9b8058748332e47093a0a391f56e36a88df64fad
targetlakshya <thetarget018@gmail.com>
2024-10-08 15:29:03 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -11,7 +11,7 @@ class Solution(object):
             if char in mapping.values():
                 stack.append(char)
             elif char in mapping.keys():
-                if not stack or stack.pop() != mapping(char):
+                if not stack or stack.pop() != mapping[char]:
                     return False
         return not stack
         pass
