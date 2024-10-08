a5e5bd2ed0d3f23e8405578813aa4e6bd44591d8
targetlakshya <thetarget018@gmail.com>
2024-10-08 15:36:46 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -13,6 +13,14 @@ class Solution(object):
             'D' : 500,
             'M' : 1000
         }
+
+        total= 0
+        n = len(s)
+
+        for i in range(n):
+            value = roman_values[s[i]]
+
+            
         pass
 
 
